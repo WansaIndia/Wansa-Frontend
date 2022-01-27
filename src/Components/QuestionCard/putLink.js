2d864735ab81d69f08bd208a79d4e38936225509
Ayush Rathore <60135991+codbreakrayush@users.noreply.github.com>
2022-01-27 12:45:03 +0530

    Code Updated

--- src/Components/QuestionCard/putLink.js
+++ src/Components/QuestionCard/putLink.js
@@ -5,15 +5,15 @@ import { Link } from "react-scroll";
 function PutLink(props){
     if(props.index>1 && props.index<=props.noOfQues && props.type==="prev")
         return(
-            <Link to={props.index-1} smooth={true} duration={400}><img src="/up.png" className="prev" alt="" width={50} /></Link>
+            <Link to={props.index-1} smooth={true} duration={400}><img src="/Images/up.png" className="prev" alt="" width={50} /></Link>
         )
     else if(props.index<props.noOfQues && props.type==="next")
         return(
-            <Link to={props.index+1} smooth={true} duration={400}><img src="/down.png" className="next" alt="" width={40} /></Link>
+            <Link to={props.index+1} smooth={true} duration={400}><img src="/Images/down.png" className="next" alt="" width={40} /></Link>
         )
     else if(props.index===props.noOfQues)
         return(
-            <Link to="sub" smooth={true} duration={400}><img src="/down.png" className="next" alt="" width={40} /></Link>
+            <Link to="sub" smooth={true} duration={400}><img src="/Images/down.png" className="next" alt="" width={40} /></Link>
         )
     return(
         <Link to="sub" smooth={true} duration={400}></Link>
