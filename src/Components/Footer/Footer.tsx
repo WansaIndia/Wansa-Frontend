631133d51cc3be0bceebda82047b4fab005c5dc1
Ayush Rathore <60135991+codbreakrayush@users.noreply.github.com>
2022-02-01 13:12:35 +0530

    Footer Update

--- src/Components/Footer/Footer.tsx
+++ src/Components/Footer/Footer.tsx
@@ -11,25 +11,25 @@ export default function Footer() {
             <p>To be growth catalyst for startups MSMEs and SMEs by offering solutions through offline ,digital ,technology & resolving their problems in Reach, Visibility ,Sales & Revenue.</p>
             </div>
             <div className="section-2">
-                <h3 className="heading">Useful Link</h3>
-                <h4 className="heading-2"><a href="#">&gt; Website and Apps</a></h4>
-                <h4 className="heading-2"><a href="#">&gt; Digital Marketing and Strategy</a></h4>
-                <h4 className="heading-2"><a href="#">&gt; Consulting go to Market</a></h4>
-                <h4 className="heading-2"><a href="#">&gt; Creative and Design</a></h4>
-                <h4 className="heading-2"><a href="#">&gt; Business Planning</a></h4>
-                <h4 className="heading-2"><a href="#">&gt; Privacy Policy</a></h4>
-                <h4 className="heading-2"><a href="#">&gt; Join as Solution Partner</a></h4>
+                <h4 className="heading">Useful Link</h4>
+                <h5 className="heading-2"><a href="#">&gt; Website and Apps</a></h5>
+                <h5 className="heading-2"><a href="#">&gt; Digital Marketing and Strategy</a></h5>
+                <h5 className="heading-2"><a href="#">&gt; Consulting go to Market</a></h5>
+                <h5 className="heading-2"><a href="#">&gt; Creative and Design</a></h5>
+                <h5 className="heading-2"><a href="#">&gt; Business Planning</a></h5>
+                <h5 className="heading-2"><a href="#">&gt; Privacy Policy</a></h5>
+                <h5 className="heading-2"><a href="#">&gt; Join as Solution Partner</a></h5>
             </div>
             <div className="section-3">
-                <h3 className="heading">Join as Solution Partner</h3>
-                <p>The  single destination for your organization to manage your business relationship and partnering opportunities with Wansa , Wansa's Clients, and other partners.</p>
-                <h3 className="heading">For Enquiries</h3>
+                <h4 className="heading">For Enquiries</h4>
                 <p>Info@wansaa.in</p>
                 <p>Contactus@wansaa.in</p>
                 <p><strong>Call us at : </strong> +91 7006184231</p>
+                <h4 className="heading">Join as Solution Partner</h4>
+                <p>The  single destination for your organization to manage your business relationship and partnering opportunities with Wansa , Wansa's Clients, and other partners.</p>
             </div>
             <div className="section-4">
-                <h3 className="heading">Our Offices</h3>
+                <h4 className="heading">Our Offices</h4>
                 {/* <div id="googleMap" className="Map"></div> */}
                 <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3503.8391949236757!2d77.31136331508131!3d28.574591082440673!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x390ce59d528f4991%3A0x11e4ee9124192932!2sWeWork%20Berger%20Delhi%20One!5e0!3m2!1sen!2sin!4v1642831494375!5m2!1sen!2sin"  loading="lazy"></iframe>
                 <div className="social-handles">
